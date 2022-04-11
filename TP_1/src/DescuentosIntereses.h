a984c4aed42f756ecd68d59ffa4f7dac0a9b9b9a
Fonte Learn <iaraflorencia2@gmail.com>
2022-04-11 11:20:48 -0300

    TP ejercicio 1-4

---
+++ TP_1/src/DescuentosIntereses.h
@@ -0,0 +1,16 @@
+/*
+ * DescuentosIntereses.h
+ *
+ *  Created on: 8 abr. 2022
+ *      Author: W7
+ */
+
+#ifndef DESCUENTOSINTERESES_H_
+#define DESCUENTOSINTERESES_H_
+
+float descuento (float numero, float precio);
+float interes (float numero, float precio);
+int descuento2 (int numero, int precio);
+int interes2 (int numero, int precio);
+
+#endif /* DESCUENTOSINTERESES_H_ */
